e6c3533fca483c403143d640c52c1a061738ceb2
saman <saman1817z@gmail.com>
2023-04-10 15:42:35 +0100

    Overloading readNumber Method also refactor the Scanner method

--- src/com/samanz/Console.java
+++ src/com/samanz/Console.java
@@ -3,12 +3,16 @@ package com.samanz;
 import java.util.Scanner;
 
 public class Console {
+    private static Scanner scanner =new Scanner(System.in);
+    public static double readNumber(String prompt){
+        return scanner.nextDouble();
+
+    }
     public static double readNumber(String prompt,double min,double max){
-        Scanner scanner = new Scanner(System.in);
         double vale;
         while (true) {
             System.out.print(prompt);
-            vale = scanner.nextFloat();
+            vale = scanner.nextDouble();
             if (vale >=min && vale<=max)
                 break;
             System.out.println("enter a value between "+ min + "and " + max);
